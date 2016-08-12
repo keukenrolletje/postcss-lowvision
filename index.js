717ecc900d31bd8e2dc91834ce8f4742fa22b5a4
Joke Gysen <jokegysen@gmail.com>
2016-08-12 13:58:56 +0200

    Learn how to write a postcss plugin

---
+++ index.js
@@ -0,0 +1,16 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-lowvision', function (opts) {
+    opts = opts || {};
+
+    // Work with options here
+
+    return function (css, result) {
+
+      // Runs through all of the nodes (declorations) in the file
+      css.walkDecls(declaration => {
+        declaration.value = declaration.value.split('').reverse().join('');
+      });
+
+    };
+});
