63b5549fc1e6d73317ab0cc32b2b287fbfd3a5f6
Joke Gysen <jokegysen@gmail.com>
2016-08-12 13:59:15 +0200

    Learn how to write a test for postcss plugin

---
+++ test.js
@@ -0,0 +1,17 @@
+import postcss from 'postcss';
+import test    from 'ava';
+
+import plugin from './';
+
+function run(t, input, output, opts = { }) {
+    return postcss([ plugin(opts) ]).process(input)
+        .then( result => {
+            t.deepEqual(result.css, output);
+            t.deepEqual(result.warnings().length, 0);
+        });
+}
+
+// This test passed.
+test('reverse color value from dlog to gold', t => {
+  return run(t, 'a{ color: dlog }', 'a{ color: gold }');
+});
