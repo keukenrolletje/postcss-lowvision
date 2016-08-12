0a564dbafc22a223da766ce675a2cd3fd385b5b8
Joke Gysen <jokegysen@gmail.com>
2016-08-12 16:43:33 +0200

    Add return function

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-lowvision', function (opts) {
     opts = opts || {};
 
-    css.walkRules(function (rule) {
-          if ( rule.selector.indexOf('html') !== -1 ) {
-                rule.append({ prop: 'color', value: 'transparent' });
-                rule.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });
-          }
-    });
+    return function (css, result) {
+      css.walkRules(function (rule) {
+            if ( rule.selector.indexOf('html') !== -1 ) {
+                  rule.append({ prop: 'color', value: 'transparent' });
+                  rule.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });
+            }
+      });
+    }
 });
