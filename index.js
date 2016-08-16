c6fb497d6dfb819192ad7d28db04259c402eb5ed
Joke Gysen <jokegysen@gmail.com>
2016-08-16 14:43:30 +0200

    Change walkAtRules to walkRules

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = postcss.plugin('postcss-lowvision', function (opts) {
     opts = opts || {};
 
     return function (css, result) {
-      css.walkAtRules('html', function (rule) {
+      css.walkRules('html', function (rule) {
         /*root.first.append({ prop: 'color', value: 'transparent' });
         root.first.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });*/
         postcss.parse('html { color: transparent; text-shadow: 0 0 5px rgba(0,0,0,1);}');
