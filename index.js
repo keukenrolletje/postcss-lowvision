c36280551697b7c962c5dc7e46b3b4a96c47833a
Joke Gysen <jokegysen@gmail.com>
2016-08-16 14:57:47 +0200

    Use cloneBefore

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ module.exports = postcss.plugin('postcss-lowvision', function (opts) {
 
     return function (css, result) {
       css.walkRules('html', function (rule) {
-        /*root.first.append({ prop: 'color', value: 'transparent' });
-        root.first.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });*/
-        postcss.parse('html { color: transparent; text-shadow: 0 0 5px rgba(0,0,0,1);}');
+        decl.cloneBefore({ prop: 'color',  decl.value: 'transparent' });
+        decl.cloneBefore({ prop: 'text-shadow',  decl.value: '0 0 5px rgba(0,0,0,1)' });
       });
     }
 });
