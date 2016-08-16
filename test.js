edb920a4d099f4abba126734329c80c170dfb6aa
Joke Gysen <jokegysen@gmail.com>
2016-08-16 16:36:57 +0200

    Change test deepequal to is

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ import plugin from './';
 function run(t, input, output) {
     return postcss([ plugin ]).process(input)
         .then( result => {
-            t.deepEqual(result.css, output);
-            t.deepEqual(result.warnings().length, 0);
+            t.is(result.css, output);
+            t.is(result.warnings().length, 0);
         });
 }
 
