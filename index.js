84cc5684032acc4ecb49907bb81a37c0966c932f
Joke Gysen <jokegysen@gmail.com>
2016-08-16 14:41:09 +0200

    Parse css

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ module.exports = postcss.plugin('postcss-lowvision', function (opts) {
     opts = opts || {};
 
     return function (css, result) {
-      css.walkRules(function (rule) {
-            if ( rule.selector.indexOf('html') !== -1 ) {
-                  root.first.append({ prop: 'color', value: 'transparent' });
-                  root.first.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });
-            }
+      css.walkAtRules('html', function (rule) {
+        /*root.first.append({ prop: 'color', value: 'transparent' });
+        root.first.append({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,1)' });*/
+        postcss.parse('html { color: transparent; text-shadow: 0 0 5px rgba(0,0,0,1);}');
       });
     }
 });
