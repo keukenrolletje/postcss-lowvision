5e458a52da475b04c1071c3c8541f212cade0786
Joke Gysen <jokegysen@gmail.com>
2016-08-16 15:15:21 +0200

    Add decl

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = postcss.plugin('postcss-lowvision', function (opts) {
     opts = opts || {};
 
     return function (css, result) {
-      css.walkRules('html', function (rule) {
+      css.walkRules('html', function (decl, rule) {
         decl.cloneBefore({ prop: 'color',  value: 'transparent' });
         decl.cloneBefore({ prop: 'text-shadow',  value: '0 0 5px rgba(0,0,0,1)' });
       });
