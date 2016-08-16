ef34e4d0d5a0bdb2d288295c400f2b46befd8b5c
Joke Gysen <jokegysen@gmail.com>
2016-08-16 16:57:53 +0200

    Clean code

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-lowvision', function (opts) {
-    opts = opts || {};
-
-    return function (css, result) {
-      css.walkRules('html', function (decl, rule) {
+module.exports = postcss.plugin('postcss-lowvision', function () {
+    return function (css) {
+      css.walkRules('html', function (decl) {
         if ( decl.selector !== -1 ){
           decl.append({ prop: 'color',  value: 'transparent' });
           decl.append({ prop: 'text-shadow',  value: '0 0 5px rgba(0,0,0,1)' });
