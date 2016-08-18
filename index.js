4bc10aa88c98f702b1be75b8cf79d8faafac9839
Joke Gysen <jokegysen@gmail.com>
2016-08-18 16:02:54 +0200

    Fix errors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-lowvision', function () {
     return function (css) {
         css.walkRules('html', function (decl) {
-          if ( decl.selector !== -1 ){
-            decl.append({ prop: 'color',  value: 'transparent' });
-            decl.append({ prop: 'text-shadow',  value: '0 0 5px rgba(0,0,0,1)' });
-          }
+            if ( decl.selector !== -1 ) {
+                decl.append({ prop: 'color',  value: 'transparent' });
+                decl.append({ prop: 'text-shadow',
+                              value: '0 0 5px rgba(0,0,0,1)' });
+            }
         });
     };
 });
