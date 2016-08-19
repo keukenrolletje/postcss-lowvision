481e961777cbf1d750ebb623d9f304020365d357
Joke Gysen <jokegysen@gmail.com>
2016-08-19 12:21:19 +0200

    Add image blur

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ module.exports = postcss.plugin('postcss-lowvision', function () {
             decl.cloneAfter({ prop: 'text-shadow',
                               value: '0 0 5px rgba(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ', ' + '1)' });
         });
+        css.walkRules('img', function (decl) {
+            decl.append({ prop: 'filter',  value: 'blur(5px)' });
+        });
     };
 });
