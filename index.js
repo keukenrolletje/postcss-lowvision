34cd265eb9284effdffb6b6314ea4632e80fbd86
Joke Gysen <jokegysen@gmail.com>
2016-08-19 11:26:59 +0200

    Test to see if colors package works

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 var postcss = require('postcss'),
-    colors = require('color');
+    color = require('color');
 
 module.exports = postcss.plugin('postcss-lowvision', function () {
     return function (css) {
         css.walkDecls('color', function (decl) {
-            var color = decl.value
-            var rgb = colors.rgb(color);
+            var val = decl.value
+            var rgb = color(val);
+            rgb.rgb();
             decl.value = rgb;
             decl.cloneAfter({ prop: 'text-shadow',
                               value: '0 0 5px rgba(0,0,0,0.5)' });
