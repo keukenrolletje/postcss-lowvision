acf118c9229c7958c6b6a0c0238d30bec219f0e8
Joke Gysen <jokegysen@gmail.com>
2016-08-19 11:16:32 +0200

    Require color

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var postcss = require('postcss');
+
+var postcss = require('postcss'),
+    colors = require('color');
 
 module.exports = postcss.plugin('postcss-lowvision', function () {
     return function (css) {
-        css.walkRules('html', function (decl) {
-            if ( decl.selector !== -1 ) {
-                decl.append({ prop: 'color',  value: 'transparent' });
-                decl.append({ prop: 'text-shadow',
-                              value: '0 0 5px rgba(0,0,0,1)' });
-            }
         css.walkDecls('color', function (decl) {
 
             decl.value = decl.value;
