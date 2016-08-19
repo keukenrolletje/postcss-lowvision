e35156db94e7f68a7fc577982c6f07e9c8b7aba7
Joke Gysen <jokegysen@gmail.com>
2016-08-19 11:16:09 +0200

    update blur function to add blur where there is color decl.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ module.exports = postcss.plugin('postcss-lowvision', function () {
                 decl.append({ prop: 'text-shadow',
                               value: '0 0 5px rgba(0,0,0,1)' });
             }
+        css.walkDecls('color', function (decl) {
+
+            decl.value = decl.value;
+            decl.cloneAfter({ prop: 'text-shadow', value: '0 0 5px rgba(0,0,0,0.5)'});
         });
     };
 });
