983ce51f645b85795f8ad038d6587f9cb6b3205d
Joke Gysen <jokegysen@gmail.com>
2016-08-22 10:15:57 +0200

    Improve options

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var postcss = require('postcss'),
     color = require('color');
 
 module.exports = postcss.plugin('postcss-lowvision', function (opts) {
-    opts = opts || 5;
+    opts = opts || {};
+    var strength = opts.strength || '5';
     return function (css) {
         // Get all css with color declaration
         css.walkDecls('color', function (decl) {
@@ -19,7 +20,7 @@ module.exports = postcss.plugin('postcss-lowvision', function (opts) {
 
             // Use text-shadow to add a blur effect and use rgb as color
             decl.cloneAfter({ prop: 'text-shadow',
-                              value: '0 0 ' + opts + 'px rgba(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ', ' + '1)' });
+                              value: '0 0 ' + strength + 'px rgba(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ', ' + '1)' });
         });
 
         // Find all filter declarations using regex to allow prefixes
